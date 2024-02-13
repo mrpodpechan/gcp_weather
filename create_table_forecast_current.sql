29b7b4b08db50f1b6d3c852d0f5f5d336042b113
mrpodpechan <105000911+mrpodpechan@users.noreply.github.com>
2024-02-13 10:12:53 -0600

    Add files via upload

---
+++ create_table_forecast_current.sql
@@ -0,0 +1,19 @@
+CREATE TABLE weather_data_current (
+    id BIGINT,
+    lat FLOAT,
+    lon FLOAT,
+    tz TEXT,
+    date DATE,
+    units TEXT,
+    current_temp FLOAT,
+    feels_like FLOAT,
+    pressure FLOAT,
+    humidity FLOAT,
+    dew_point FLOAT,
+    uvi FLOAT,
+    clouds FLOAT,
+    visibility FLOAT,
+    wind_speed FLOAT,
+    wind_deg FLOAT,
+    wind_gust FLOAT
+);
